b89cc73822b0a7e1d56ff5f6494710a426232e6f
Jack Liu <jackliu@haneco.local>
2024-09-26 15:43:09 +0930

    1

--- src/config.js
+++ src/config.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
-const baseURL = 'localhost:8080/api';
+const baseURL = '/api';
 
-const getTimestamp = () => {
-  const now = new Date();
-  return now.toISOString(); // 使用 ISO 格式的时间戳
-};
-
-console.log(`[${getTimestamp()}] Testing3 baseURL:`, baseURL);
+console.log(`Testing4 baseURL:`, baseURL);
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
