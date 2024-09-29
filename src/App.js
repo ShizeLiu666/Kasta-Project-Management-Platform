46cdab551e29745a144bac5639cfe4a9effdfd88
shizeliu0408 <a1844323@student.adelaide.edu.au>
2024-09-29 15:30:54 +0930

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import ThemeRoutes from "./routes/Router";
+import "./assets/scss/App.css";
+// import { ExcelConverterProvider } from './components/fileConverter/ExcelConverterContext';
+
+const AppRoutes = () => {
+  const routing = useRoutes(ThemeRoutes);
+  return routing;
+};
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        {ThemeRoutes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            element={route.element}
-          />
-        ))}
-      </Routes>
+      <div className="dark">
+        <AppRoutes />
+      </div>
     </Router>
   );
 };
