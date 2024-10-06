4bef6483472c43425d3c016f789a61db928c8a49
shizeliu0408 <a1844323@student.adelaide.edu.au>
2024-10-06 22:57:29 +1030

    1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/Router";
+import React, { useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Router";
 import "./assets/scss/App.css";
 
 const App = () => {
+  useEffect(() => {
+    const isFirstLoad = sessionStorage.getItem('isFirstLoad') !== 'false';
+    if (isFirstLoad) {
+      sessionStorage.setItem('isFirstLoad', 'false');
+      window.location.reload();
+    }
+  }, []);
+
   return (
-    <Router>
-      <div className="dark">
-        <AppRoutes />
-      </div>
-    </Router>
+    <div className="dark">
+      <RouterProvider router={router} />
+    </div>
   );
 };
 
