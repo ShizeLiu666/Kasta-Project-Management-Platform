b3ac7843b72f4568949ae561d875c15a59725536
shizeliu0408 <a1844323@student.adelaide.edu.au>
2024-10-06 23:15:15 +1030

    1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router";
 import "./assets/scss/App.css";
 
 const App = () => {
-  useEffect(() => {
-    const isFirstLoad = sessionStorage.getItem('isFirstLoad') !== 'false';
-    if (isFirstLoad) {
-      sessionStorage.setItem('isFirstLoad', 'false');
-      window.location.reload();
-    }
-  }, []);
-
   return (
     <div className="dark">
       <RouterProvider router={router} />
