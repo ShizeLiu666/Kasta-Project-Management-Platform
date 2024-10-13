8c7f414732bf786584317b7eaf6e0a7f04637b34
shizeliu0408 <a1844323@student.adelaide.edu.au>
2024-10-13 11:21:43 +1030

    12102024

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router";
 import "./assets/scss/App.css";
@@ -6,9 +5,7 @@ import "./assets/scss/App.css";
 const App = () => {
   return (
     <div className="dark">
-      <Suspense fallback={<div>加载中...</div>}>
-        <RouterProvider router={router} />
-      </Suspense>
+      <RouterProvider router={router} />
     </div>
   );
 };
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { Navigate } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
 
@@ -11,15 +11,21 @@ const Projects = lazy(() => import("../components/projects/ProjectList/ProjectLi
 const Profile = lazy(() => import("../components/profile/profile"));
 const AuthCodeManagement = lazy(() => import("../components/AuthCodeManagement/AuthCodeManagement"));
 
+// const Form = lazy(() => import("../views/ui/Form.js"));
+// const About = lazy(() => import("../views/About.js"));
+// const Alerts = lazy(() => import("../views/ui/Alerts"));
+// const Badges = lazy(() => import("../views/ui/Badges"));
+// const Buttons = lazy(() => import("../views/ui/Buttons"));
+// const Grid = lazy(() => import("../views/ui/Grid"));
+// const Tables = lazy(() => import("../views/ui/Tables"));
+// const ExcelConverter = lazy(() => import("../components/fileConverter/ExcelConverter"));
+// const Breadcrumbs = lazy(() => import("../views/ui/Breadcrumbs"));
+
 /*****Routes******/
 const router = createBrowserRouter([
   {
     path: "/login",
-    element: (
-      <Suspense fallback={<div>加载中...</div>}>
-        <LoginPage />
-      </Suspense>
-    ),
+    element: <LoginPage />,
   },
   {
     path: "/",
@@ -27,36 +33,20 @@ const router = createBrowserRouter([
   },
   {
     path: "/admin",
-    element: (
-      <Suspense fallback={<div>加载中...</div>}>
-        <FullLayout />
-      </Suspense>
-    ),
+    element: <FullLayout />,
     children: [
-      {
-        path: "projects",
-        element: (
-          <Suspense fallback={<div>加载中...</div>}>
-            <Projects />
-          </Suspense>
-        ),
-      },
-      {
-        path: "profile",
-        element: (
-          <Suspense fallback={<div>加载中...</div>}>
-            <Profile />
-          </Suspense>
-        ),
-      },
-      {
-        path: "auth-code-management",
-        element: (
-          <Suspense fallback={<div>加载中...</div>}>
-            <AuthCodeManagement />
-          </Suspense>
-        ),
-      },
+      { path: "projects", element: <Projects /> },
+      { path: "profile", element: <Profile /> },
+      { path: "auth-code-management", element: <AuthCodeManagement /> },
+      // { path: "about", element: <About /> },
+      // { path: "alerts", element: <Alerts /> },
+      // { path: "badges", element: <Badges /> },
+      // { path: "buttons", element: <Buttons /> },
+      // { path: "grid", element: <Grid /> },
+      // { path: "table", element: <Tables /> },
+      // { path: "excelconverter", element: <ExcelConverter /> },
+      // { path: "breadcrumbs", element: <Breadcrumbs /> },
+      // { path: "forms", element: <Form /> },
     ],
   },
   {
