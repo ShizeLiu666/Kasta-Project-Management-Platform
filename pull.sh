fc5f90337501d2f86e707e653db56f5db6bfb94e
Jack Liu <jackliu@haneco.local>
2024-10-14 14:39:19 +1030

    14102024 2:38 minor changes on pull bash script

--- pull.sh
+++ pull.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 # /Kasta-Project-Management-Platform 下做以下操作更新代码
+
 # 下拉最新版本
 git pull origin main
 
 # 生成静态文件
 npm run build
 
-# 删除旧的前端构建文件夹
-sudo rm -rf /var/www/html/build
+# 删除旧的前端构建文件夹 (指向 /srv/react-app/build)
+sudo rm -rf /srv/react-app/build
 
-# 将新构建的 'build' 文件夹复制到服务器的 Web 根目录 (/var/www/html/)
-sudo cp -r /root/Kasta-Project-Management-Platform/build /var/www/html/
+# 将新构建的 'build' 文件夹移动到 /srv/react-app/
+sudo mv /root/Kasta-Project-Management-Platform/build /srv/react-app/
 
-# 清除 Nginx 缓存
+# 清除 Nginx 缓存 (如果有缓存路径)
 sudo rm -rf /data/nginx/cache/*
 
 # 重启 Nginx 服务，使新的构建文件生效
